36e1420b1f40423da04b0d379ec4506e1f0df9d1
Yazmin82 <yazdre@hotmail.com>
2025-04-28 14:43:32 -0600

    Update Adivinarelnumero.java

--- Adivinarelnumero.java
+++ Adivinarelnumero.java
@@ -1,16 +1,15 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * C/*lick nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package adivinarelnumero;
-import java.util.Scanner;
-import java.util.Random;
+package adivelnumero;
 
 /**
  *
  * @author alfa
- */
-public class Adivinarelnumero {
+ */import java.util.Scanner;
+   import java.util.Random;
+public class Adivelnumero {
 
     /**
      * @param args the command line arguments
@@ -18,30 +17,26 @@ public class Adivinarelnumero {
     public static void main(String[] args) {
         // TODO code application logic here
          Random random = new Random();
-        int numeroSecreto = random.nextInt(100) + 1; 
-
+         int numeroSecreto = random.nextInt(100) + 1;
         Scanner Scanner = new Scanner(System.in);
-        int intento;
-        int contadorIntentos = 0;
-         System.out.println("¡Bienvenido al juego de adivinar el número!");
-        System.out.println("Estoy pensando en un número entre 1 y 100.");
-
+           int intento;
+              System.out.println(" ¡Adivina un numero entre 1 y 100!");
+         
         do {
-            System.out.print("Introduce tu suposición: ");
-            intento = Scanner.nextInt();
-            contadorIntentos++;
+          System.out.print("Ingresa el numero: ");
+          intento = Scanner.nextInt();
 
-            if (intento < numeroSecreto) {
-                System.out.println("Demasiado bajo. Intenta de nuevo.");
-            } else if (intento > numeroSecreto) {
-                System.out.println("Demasiado alto. Intenta de nuevo.");
-            } else {
-                System.out.println("¡Correcto! Has adivinado el número en " + contadorIntentos + " intentos.");
-            }
-
-        } while (intento != numeroSecreto);
+         if (intento < numeroSecreto) {
+             System.out.println("el numero es bajo.Intenta de nuevo.");
+       } else if (intento > numeroSecreto) {
+           System.out.println("El numero es alto. Intenta de nuevo.");
+         } else {
+          System.out.println("¡muy bien ! Este es el numero.");
+       }
+         } while (intento != numeroSecreto);
 
         Scanner.close();
-    }
-    
 }
+
+}
+
