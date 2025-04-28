57c1b7cac1209605477665a2510a81b67da5e816
Yazmin82 <yazdre@hotmail.com>
2025-04-28 15:00:58 -0600

    Update Secuenciaaritmetica.java

--- Secuenciaaritmetica.java
+++ Secuenciaaritmetica.java
@@ -2,42 +2,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package secuenciaaritmetica;
-
+package secdecuadrados;
 
 /**
  *
  * @author alfa
- */
-import java.util.Scanner;
-public class Secuenciaaritmetica {
+ */import java.util.Scanner;
+public class Secdecuadrados {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         Scanner Scanner = new Scanner(System.in);
- System.out.print("Ingrese el primer número de la secuencia: ");
-        int primerNumero = Scanner.nextInt();
-
-        System.out.print("Ingrese la diferencia entre los números: ");
-        int diferencia = Scanner.nextInt();
-
-        System.out.print("Ingrese el número máximo de la secuencia: ");
-        int numeromaximo = Scanner.nextInt();
-
-        int numeroActual = primerNumero;
-
-        System.out.println("Secuencia aritmética:");
-
+        Scanner Scanner = new Scanner(System.in);
+        int numero;
+        System.out.print("Ingresa un numero entero positivo: ");
+        numero = Scanner.nextInt();
+        if (numero > 0)
+        { int i = 1;
+        System.out.println("Los cuadrados de los numeros desde 1 hasta " + numero + "son:");
         do {
-            System.out.print(numeroActual);
-            numeroActual += diferencia;
-        } while (numeroActual <= numeromaximo);
-
+            int cuadrado = i * i;
+            System.out.println("El cuadrado de " + i + " es " + cuadrado); 
+            i++;
+        } while (i <= numero);
+        } else {
+            System.out.println("Por favor, ingresa un numero positivo.");
+        }
         Scanner.close();
+
     }
+    
+}
 
     }
 
