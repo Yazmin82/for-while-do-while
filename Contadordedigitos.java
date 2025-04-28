391b5432c4b93b74e8167f33949131ae0cad8b58
Yazmin82 <yazdre@hotmail.com>
2025-04-28 14:47:33 -0600

    Update Contadordedigitos.java

--- Contadordedigitos.java
+++ Contadordedigitos.java
@@ -2,43 +2,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package contadordedigitos;
-import java.util.Scanner;
-import java.util.Random;
+package condedigitos;
 
 /**
  *
  * @author alfa
- */
-public class Contadordedigitos {
+ */import java.util.Scanner; 
+public class Condedigitos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner scanner = new Scanner(System.in);
-         Random random = new Random();
-          int numeroAleatorio = random.nextInt(100) + 1;
-           int suposicion = 0;
-            System.out.println("¡Bienvenido al juego de adivinanza!");
-        System.out.println("Estoy pensando en un número entre 1 y 100.");
-        System.out.println("Intenta adivinarlo.");
-         while (suposicion != numeroAleatorio) {
-              System.out.print("Ingresa tu suposición: ");
-            suposicion = scanner.nextInt();
-             if (suposicion < numeroAleatorio) {
-                System.out.println("¡Demasiado bajo! Intenta de nuevo.");
-            } else if (suposicion > numeroAleatorio) {
-                System.out.println("¡Demasiado alto! Intenta de nuevo.");
-            } else {
-                System.out.println("¡Felicidades! Has adivinado el número.");
-                scanner.close();
-                
-         }
-         }
-    }
-}
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.print("Ingresa un numero entero: "); 
+        int numero = Scanner.nextInt();
+        
+        if (numero < 0) { numero = -numero;
+        }
+         
+        int contador = 0;
+        if (numero == 0) {
+
+            contador = 1;
+       } else {
+             while (numero > 0) {
+                 numero = numero / 10; contador++;
     
+         System.out.println("El numero tiene " + contador + " digitos.");
+
+
+         Scanner.close();
+             }
+        }
+    }
+
+
+ }
+
+
 
 
