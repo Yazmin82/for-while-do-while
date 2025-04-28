e08992490c7785dadbcee3d9c290894917fd2265
Yazmin82 <yazdre@hotmail.com>
2025-04-28 14:55:19 -0600

    Update Vocalesonovocales.java

--- Vocalesonovocales.java
+++ Vocalesonovocales.java
@@ -2,53 +2,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package vocalesonovocales;
-
+package vocalono;
 
 /**
  *
  * @author alfa
- */import java.util.Scanner;
-public class Vocalesonovocales {
+ */import java.util.Scanner; 
+public class Vocalono {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // TODO code application logic here
         Scanner scanner = new Scanner(System.in);
         char letra;
-
-        System.out.println("Ingresa letras. El programa finalizará cuando ingreses un espacio.");
-
+        System.out.println("El programa terminará con un espacio:");
         while (true) {
             System.out.print("Ingresa una letra: ");
             String entrada = scanner.nextLine();
-            if (entrada.length() != 1) {
-                System.out.println(" Ingresa una letra.");
+            if (entrada.isEmpty()) {
+                System.out.println("Intenta de nuevo.");
                 continue;
             }
-            letra = entrada.charAt(0);
+             letra = entrada.charAt(0); 
             if (letra == ' ') {
-                System.out.println("finalizado.");
-                break;
-            }
+            System.out.println("Fin del programa");
+            break;
+        }
             if (!Character.isLetter(letra)) {
-                System.out.println("No es una vocal.");
-                continue;
-            }
-            letra = Character.toLowerCase(letra);
-            if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'||
-                    letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
-                System.out.println("Es una vocal.");
+            System.out.println(" No es una letra");
+             continue;
+        }
+            char letraMinuscula = Character.toLowerCase(letra);
+            if (letraMinuscula == 'a' || letraMinuscula == 'e' || letraMinuscula == 'i' ||
+            letraMinuscula == 'o' || letraMinuscula == 'u') {
+            System.out.println("Es una vocal.");
             } else {
                 System.out.println("Es una consonante.");
             }
         }
-
-        scanner.close();
+    }
 }
-}
-
 
 
         
